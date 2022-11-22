bdb1232ae495dafee5a09e4393e8917555cf01c3
Zness1 <eziola@gmail.com>
2022-11-21 18:07:56 -0700

    named tuples2

--- named_tuples.py
+++ named_tuples.py
@@ -2,8 +2,9 @@
 
 from collections import namedtuple
 
-Color = namedtuple('Color', ['red', 'green','blue'])
+Color = namedtuple('Color', ['red', 'green', 'blue'])
 
-c = Color(55,155,255)
-
+c = Color(55, 155, 255)
+white = Color(255, 255, 255)
 print(c.red)
+print(white.green)
